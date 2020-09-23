402f018be8fea14be69ecb4a18f6319d11c84e6d
BuildTools <unconfigured@null.spigotmc.org>
2020-09-23 11:04:27 -0600

    Added LICENSE and included an @copyright in the JS files.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Pathfinding2D-Example
-This project is an HTML/JS page for viewing and simulating well-known pathfinding algorithms. It was originally part of my personal site, but I would like have the ability to commit to it separately, which is why it is being moved to its own repository.
+This project is an HTML/JS page for viewing and simulating well-known pathfinding algorithms.
+It was originally part of my personal site, but I would like have the ability to commit to it separately, which is why it is being moved to its own repository.
+
+This project is being built using the Model-View-Controller concept.
+Although this was not the case at the start of the project, changes are being made to convert it to this form.
+By looking at the source code, one can see that many of the elements and interactions feels like spaghetti.
+This is due to the last change over to MVC.
+
+It is my hope that, by the time this project is completed, I will develop a stronger understanding of the various
+approaches to solving shortest-path problems and that I will also have developed a fun little program for people to
+mess around with when they are bored.
--- _scripts/Model.js
+++ _scripts/Model.js
@@ -8,7 +8,24 @@
  * Positions within the matrix are referenced as an (x, y) pair
  * - this pair is 0-based and measured from the upper left corner
  *
+ * @copyright
+ * Pathfinding2D-Example: An MVC approach to an HTML pathfinding visualizer.
+ * Copyright (C) 2020 Jacob Loosa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 class Model {
 
     lastChangeTime = Date.now()
@@ -51,6 +68,7 @@ class Model {
      * Creates a new Model
      * @param {number} width The width of the model's 2D tile map
      * @param {number} height The height of the model's 2D tile map
+     * @constructor
      */
     constructor(width, height) {
 
